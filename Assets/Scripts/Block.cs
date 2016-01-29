b1ce1e696784a298060259aa5e477219c812adeb
Stefan Andersson <stf.andersson@gmail.com>
2016-01-29 22:30:01 +0100

    Added tracking of the highest point.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,6 +1,19 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class Block : MonoBehaviour {
 
+	// public statics
+	public static List<Block> AllBlocks = new List<Block>();
+
+	// publics
 	public bool Interactible = false;
+
+
+	void OnEnable(){
+		AllBlocks.Add (this);
+	}
+	void OnDisable(){
+		AllBlocks.Remove (this);
+	}
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	// public statics
+	public static GameManager Instance;
+	public static float HighestPoint = 0;
+
+	// privates
+	private const float UPDATE_LATENCY = 0.1f;
+	private IEnumerator updateHighestPointRoutine;
+
+
+	void Awake(){
+		if (Instance) Destroy(this);
+		else Instance = this;
+	}
+	void OnEnable(){
+		updateHighestPointRoutine = _UpdateHighestPoint();
+		StartCoroutine(updateHighestPointRoutine);
+	}
+	void OnDisable(){
+		StopCoroutine(updateHighestPointRoutine);
+	}
+
+	private static IEnumerator _UpdateHighestPoint(){
+		while (true) {
+			GetHighestPoint();
+			yield return new WaitForSeconds(UPDATE_LATENCY);
+		}
+	}
+	public static float GetHighestPoint(){
+		float highY = 0;
+		for (int i = 0; i < Block.AllBlocks.Count; i++) {
+			PolygonCollider2D pColl = Block.AllBlocks[i].GetComponent<PolygonCollider2D>();
+			for (int o = 0; o < pColl.points.Length; o++) {
+				float y = pColl.points[i].y + pColl.transform.position.y;
+				if (y > highY) {
+					highY = y;
+				}
+			}
+		}
+		
+		HighestPoint = highY;
+		return HighestPoint;
+	}
+}
