a4b5ab8abfdce26700d12c858c65a7ae066d58d3
Stefan Andersson <stf.andersson@gmail.com>
2016-01-29 22:42:57 +0100

    GameManager now keeps track of min-max X.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,12 @@ public class GameManager : MonoBehaviour {
 	// public statics
 	public static GameManager Instance;
 	public static float HighestPoint = 0;
+	public static float HighestX = 0;
+	public static float LowestX = 0;
 
 	// privates
 	private const float UPDATE_LATENCY = 0.1f;
-	private IEnumerator updateHighestPointRoutine;
+	private IEnumerator updateBoundariesRoutine;
 
 
 	void Awake(){
@@ -18,16 +20,40 @@ public class GameManager : MonoBehaviour {
 		else Instance = this;
 	}
 	void OnEnable(){
-		updateHighestPointRoutine = _UpdateHighestPoint();
-		StartCoroutine(updateHighestPointRoutine);
+		updateBoundariesRoutine = _UpdateBoundaries();
+		StartCoroutine(updateBoundariesRoutine);
 	}
 	void OnDisable(){
-		StopCoroutine(updateHighestPointRoutine);
+		StopCoroutine(updateBoundariesRoutine);
 	}
 
-	private static IEnumerator _UpdateHighestPoint(){
+	private static IEnumerator _UpdateBoundaries(){
 		while (true) {
-			GetHighestPoint();
+
+			float highY = 0;
+			float highX = 0;
+			float lowX = 0;
+
+			for (int i = 0; i < Block.AllBlocks.Count; i++) {
+				PolygonCollider2D pColl = Block.AllBlocks[i].GetComponent<PolygonCollider2D>();
+				for (int o = 0; o < pColl.points.Length; o++) {
+					Vector2 v = pColl.points[i] + pColl.transform.position;
+					if (v.y > highY) {
+						highY = v.y;
+					}
+					if (v.x > highX) {
+						highX = v.x;
+					}
+					if (v.x < lowX) {
+						lowX = v.x;
+					}
+				}
+			}
+			
+			HighestPoint = highY;
+			HighestX = highX;
+			LowestX = lowX;
+
 			yield return new WaitForSeconds(UPDATE_LATENCY);
 		}
 	}
@@ -45,5 +71,35 @@ public class GameManager : MonoBehaviour {
 		
 		HighestPoint = highY;
 		return HighestPoint;
+	}
+	public static float GetHighestX(){
+		float highX = 0;
+		for (int i = 0; i < Block.AllBlocks.Count; i++) {
+			PolygonCollider2D pColl = Block.AllBlocks[i].GetComponent<PolygonCollider2D>();
+			for (int o = 0; o < pColl.points.Length; o++) {
+				float x = pColl.points[i].x + pColl.transform.position.x;
+				if (x > highX) {
+					highX = x;
+				}
+			}
+		}
+		
+		HighestX = highX;
+		return HighestX;
+	}
+	public static float GetLowestX(){
+		float lowX = 0;
+		for (int i = 0; i < Block.AllBlocks.Count; i++) {
+			PolygonCollider2D pColl = Block.AllBlocks[i].GetComponent<PolygonCollider2D>();
+			for (int o = 0; o < pColl.points.Length; o++) {
+				float x = pColl.points[i].x + pColl.transform.position.x;
+				if (x < lowX) {
+					lowX = x;
+				}
+			}
+		}
+		
+		LowestX = lowX;
+		return LowestX;
 	}
 }
