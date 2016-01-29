68c83f5b8ab6e11c9b3a36fc73c5a4d06b88bef3
tobnyl <tobnyl@gmail.com>
2016-01-29 20:37:35 +0100

    Started on Camera script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject TowerTop;
+    public float Speed = 5f;
+    public float SmoothTime = 0.15f;
+
+    private Vector3 _moveAmount;
+    private Vector3 _smoothMoveVelocity;
+
+
+    void Start ()
+    {
+	
+	}
+
+	void Update ()
+	{
+	    var moveDir = new Vector3(
+	        0,
+            Input.GetAxisRaw("Vertical"),
+	        0).normalized;
+
+        Vector3 targetMoveAmount = moveDir * Speed;
+        _moveAmount = Vector3.SmoothDamp(_moveAmount, targetMoveAmount, ref _smoothMoveVelocity, SmoothTime);
+
+        Camera.main.transform.position += _moveAmount;
+	}
+}
