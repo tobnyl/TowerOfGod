ef37229338f8eed85b610a4e55b146caaf0d23e5
tobnyl <tobnyl@gmail.com>
2016-01-29 21:12:46 +0100

    Stop camera at y: 0

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -9,16 +9,26 @@ public class CameraMovement : MonoBehaviour
 
     private Vector3 _moveAmount;
     private Vector3 _smoothMoveVelocity;
-
+    private float _heightOver2;
+    private float _widthOver2;
 
     void Start ()
     {
-	
-	}
+        _heightOver2 = Screen.height/2f;
+        _widthOver2 = Screen.width/2f;
+
+    }
 
 	void Update ()
 	{
-	    var moveDir = new Vector3(
+	    var cameraPosition = Camera.main.transform.position;
+
+	    var bottomViewport = Camera.main.ViewportToWorldPoint(new Vector3(0, 0));
+	    var topViewport = Camera.main.ViewportToWorldPoint(new Vector3(1, 1));
+	    var viewPortWorldSize = topViewport - bottomViewport;
+        Debug.Log(viewPortWorldSize);
+
+        var moveDir = new Vector3(
 	        0,
             Input.GetAxisRaw("Vertical"),
 	        0).normalized;
@@ -27,5 +37,14 @@ public class CameraMovement : MonoBehaviour
         _moveAmount = Vector3.SmoothDamp(_moveAmount, targetMoveAmount, ref _smoothMoveVelocity, SmoothTime);
 
         Camera.main.transform.position += _moveAmount;
-	}
+
+        if (Camera.main.transform.position.y - viewPortWorldSize.y / 2f < 0)
+        {
+            Debug.Log("Camera.y < 0");
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, viewPortWorldSize.y / 2f, Camera.main.transform.position.z);
+        }
+
+
+        //Camera.main.transform.position = cameraPosition;
+    }
 }
