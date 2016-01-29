d11b720cd4125539bb4d4b6a11d1873f3c92c3c8
Max Hellqvist <thexvw@gmail.com>
2016-01-29 21:26:56 +0100

    Added Script for spawning blocks. Currently spawns by pressing Space.

--- Assets/Scripts/SpawnBlock.cs
+++ Assets/Scripts/SpawnBlock.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class SpawnBlock : MonoBehaviour {
+	TestBlockScript TBS;
+	public GameObject[] blockList;
+	public GameObject nextSpawnPoint;
+	public GameObject currentSpawnPoint;
+	public GameObject nextBlockPrefab;
+	public GameObject currentBlock;
+	public int bx;
+	public int nextBlock;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		BlockSpawn ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Space)){
+			BlockSpawn ();
+		}
+				
+	}
+	public void BlockSpawn(){
+		if (currentBlock != null) {
+			currentBlock.transform.position = currentSpawnPoint.transform.position;
+			TBS = currentBlock.GetComponent<TestBlockScript>();
+			TBS.interactable = true;
+		}
+		bx = Random.Range (0, blockList.Length);
+		nextBlockPrefab = blockList [bx];
+		nextBlock = bx;
+		currentBlock = Instantiate(nextBlockPrefab, nextSpawnPoint.transform.position, Quaternion.identity) as GameObject;
 	}
 }
+
