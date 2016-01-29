7a31b22a09832f10cc6bd88985575a8154326775
tobnyl <tobnyl@gmail.com>
2016-01-29 22:22:41 +0100

    Camera movement done

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour
 {
     public GameObject TowerTop;
+    public GameObject TowerLeftBlock;
+    public GameObject TowerRightBlock;
     public float Speed = 5f;
     public float SmoothTime = 0.15f;
 
@@ -11,25 +13,24 @@ public class CameraMovement : MonoBehaviour
     private Vector3 _smoothMoveVelocity;
     private float _heightOver2;
     private float _widthOver2;
+    private Vector3 _viewPortWorldSize;
 
     void Start ()
     {
         _heightOver2 = Screen.height/2f;
         _widthOver2 = Screen.width/2f;
 
+        var bottomViewport = Camera.main.ViewportToWorldPoint(new Vector3(0, 0));
+        var topViewport = Camera.main.ViewportToWorldPoint(new Vector3(1, 1));
+        _viewPortWorldSize = topViewport - bottomViewport;
     }
 
 	void Update ()
 	{
 	    var cameraPosition = Camera.main.transform.position;
 
-	    var bottomViewport = Camera.main.ViewportToWorldPoint(new Vector3(0, 0));
-	    var topViewport = Camera.main.ViewportToWorldPoint(new Vector3(1, 1));
-	    var viewPortWorldSize = topViewport - bottomViewport;
-        Debug.Log(viewPortWorldSize);
-
         var moveDir = new Vector3(
-	        0,
+            Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical"),
 	        0).normalized;
 
@@ -38,16 +39,30 @@ public class CameraMovement : MonoBehaviour
 
         cameraPosition += _moveAmount;
 
-        if (cameraPosition.y - viewPortWorldSize.y / 2f < 0)
+        // Vertical
+        if (cameraPosition.y - _viewPortWorldSize.y / 2f < 0)
         {
-            cameraPosition.y = viewPortWorldSize.y/2f;
+            cameraPosition.y = _viewPortWorldSize.y/2f;
         }
         else if (cameraPosition.y > TowerTop.transform.position.y)
         {
             cameraPosition.y = TowerTop.transform.position.y;
         }
 
+        // Horizontal
+	    var towerLeftBlock = TowerLeftBlock.transform.position;
+	    var towerRightBlock = TowerRightBlock.transform.position;
 
-        Camera.main.transform.position = cameraPosition;
+        if (cameraPosition.x < towerLeftBlock.x)
+        {
+            cameraPosition.x = towerLeftBlock.x;
+        }
+        else if (cameraPosition.x > towerRightBlock.x)
+        {
+            cameraPosition.x = towerRightBlock.x;
+        }
+
+
+	    Camera.main.transform.position = cameraPosition;
     }
 }
