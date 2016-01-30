ec2500d00b28c613003c3ff01e4f45753e282863
hermieboi <herman_cool@hotmail.com>
2016-01-30 12:01:52 +0100

    Found promising background sound and background picture. :)))))

---
+++ Assets/Scripts/MusicAndSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicAndSound : MonoBehaviour 
+{
+	public AudioClip audioClip;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
