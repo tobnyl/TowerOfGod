7ce40f10d51dacbf663d15caecebfc9693634288
Stefan Andersson <stf.andersson@gmail.com>
2016-01-30 23:51:37 +0100

    Tweaked some blocks and added a sunrise.

---
+++ Assets/Scripts/ColorLerper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ColorLerper : MonoBehaviour {
+
+	public Material TargetMaterial;
+	public Camera TargetCamera;
+	public Color StartColor;
+	public Color EndColor;
+	public Color CameraStartColor;
+	public Color CameraEndColor;
+	public float LerpTime;
+
+	private float t;
+
+
+	void Update () {
+		TargetMaterial.color = Color.Lerp(StartColor, EndColor, t / LerpTime);
+		TargetCamera.backgroundColor = Color.Lerp(CameraStartColor, CameraEndColor, t / LerpTime);
+
+		t += Time.deltaTime;
+
+		if (t >= LerpTime) {
+			Destroy(gameObject);
+		}
+	}
+}
