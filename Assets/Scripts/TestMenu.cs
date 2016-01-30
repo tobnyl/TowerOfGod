cd0bdd5011eff85a58bcfb44e7a56e225c425ec1
hermieboi <herman_cool@hotmail.com>
2016-01-30 16:43:51 +0100

    Test start menu, load main evel script.

---
+++ Assets/Scripts/TestMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MainLevel ()
+	{
+		Application.LoadLevel(1);
+	}
+}
